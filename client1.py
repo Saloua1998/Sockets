6725e46e1bf4e1054cb9565a73f7200b0ae30b33
Gizemsaz <g.sazak@live.nl>
2019-11-01 19:25:34 +0100

    DONE

--- client1.py
+++ client1.py
@@ -1,26 +1,42 @@
+###################################################
+# Name:          Gizem Sazak     Saloua Oua-ali   #
+# Studentnumber: 0965662         0958654          #
+# Classname:     INF2D           INF2D            #
+###################################################
+
+
 import socket
 import json
 
 # AF_INET = IPv4 and SOCK_STREAM = TCP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# connect to the server IP and port
+# connect to the serverclient IP and port
 s.connect(('145.24.222.103', 8001))
 
+#it sends object to server
 def sendObject():
     data = {"studentnr": input("studentnr: "), "classname": input("classname: "), "clientid": int(input("clientid: ")), "teamname": input("teamname: "), "ip": input("ip: "), "secret": None, "status": None}
     d = json.dumps(data)
     s.send(bytes(d, "utf-8"))
 
-def sendObjectFurther():
+#it sends object to client 2
+def sendObjectFurther(data):
     # AF_INET = IPv4 and SOCK_STREAM = TCP
     toClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # connect to the server IP and port
+    # connect to the serverclient IP and port
     toClient.connect((ipClient2, 1234))
-    toClient.send(bytes(newdata, "utf-8"))
+    toClient.send(bytes(data, "utf-8"))
 
+def getIP():
+    hostname = socket.gethostname() 
+    ip = socket.gethostbyname(hostname)
+    return ip 
 
+client1=None
+ipClient2 =""
 
-
+nameClient = input("client 1 or 2? ")
+print("Your IP: " + getIP())
 
 while True:
     # 1000 is the buffersize
@@ -28,38 +44,38 @@ while True:
     msg = msg.decode("utf-8")
     # decode the received message and print it out
     if msg:
-        if msg.startswith('{'):
-            status = json.loads(msg)["status"]
-            print(msg)
-            # If status == "waiting for message 2" than send to server
-            if (status == 'waiting for message 2'):
-             # s.send(bytes(status, "utf-8"))
-             newdata = msg
-             sendObjectFurther()
+       
+        print(msg)
+        if (msg.startswith('{') and client1==True):         
+            sendObjectFurther(msg)
         else:
-            print(msg)
-            nameClient = input("client 1 or 2? ")
             if (nameClient=="1"):
                 ipClient2 = input("IP of client 2: ")
+                client1=True
                 sendObject()
-            else: 
-                server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                server.bind((socket.gethostname(), 1234))
-                server.listen(5)
+            else:
+                client1=False
+                #client 2 becomes server
+                serverclient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                serverclient.bind((socket.gethostname(), 1234))
+                serverclient.listen(5)
+                
                 while True:
                     # store the clientsocket object in clientsocket
                     # store the client IP address in address
-                    clientsocket, address = server.accept()
+                    clientsocket, address = serverclient.accept()
                     # This is only a check to know is we're connected
                     msg = clientsocket.recv(1000)
                     msg = msg.decode("utf-8")
                     if msg:
+                    #it sends to server incl server&secret     
                         print(msg)
                         msg = json.loads(msg)
                         msg.update({"studentnr": input("studentnr: "), "clientid": int(input("clientid: ")), "ip": input("ip: ")})
                         d = json.dumps(msg)
                         s.send(bytes(d, "utf-8"))
                         break
+                        
 
             
 
